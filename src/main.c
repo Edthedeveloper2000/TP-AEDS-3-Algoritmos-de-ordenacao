b58047adc9c2f9f299e22d450d02f6a464f992aa
Ed-Games <edmarcosfilho2015@gmail.com>
2023-11-21 20:21:33 -0300

    feat: configurado utf e adicionado caso de teste bubblesort

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <locale.h>
+#include "./sorting/bubbleSort/bubble.h"
 
 int main() {
+    setlocale(LC_ALL, "en_US.utf8");
+     Card arr[] = {{VERMELHO, "2", 3, 2}, {AZUL, "3", 4, 3}, {VERDE, "3", 1, 3}, {VERMELHO, "1", 3, 1}, {VERDE, "1", 1, 1}};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    
+    /*printf("Bubble Sort");
+    for (int x = 0; x < n; x ++){
+        showCard(arr);
+    }*/
     
 }
