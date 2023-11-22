8f7d524c8681a67864266f64ddce01f83383ce6b
Ed-Games <edmarcosfilho2015@gmail.com>
2023-11-22 18:18:48 -0300

    feat: recebendo o payload do sort e usando a função de print do payload para exibir os dados da execução

--- src/main.c
+++ src/main.c
@@ -7,7 +7,10 @@ int main() {
     setlocale(LC_ALL, "en_US.utf8");
      Card arr[] = {{VERMELHO, "2", 3, 2}, {AZUL, "3", 4, 3}, {VERDE, "3", 1, 3}, {VERMELHO, "1", 3, 1}, {VERDE, "1", 1, 1}};
     int n = sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
+    SortingPayload payload = bubbleSort(arr, n);
+
+    printf("Bubble Sort:\n ");
+    showSortingPayload(&payload);
     
     /*printf("Bubble Sort");
     for (int x = 0; x < n; x ++){
