2bcf04394a0acaaf1d31b2f8e8f6f6aaca6a1f1f
Ed-Games <edmarcosfilho2015@gmail.com>
2023-11-22 23:02:14 -0300

    feat: chamando a função de menu em vez do caso de testes mocked

--- src/main.c
+++ src/main.c
@@ -1,20 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
-#include "./sorting/bubbleSort/bubble.h"
+#include "menu.h"
 
 int main() {
     setlocale(LC_ALL, "en_US.utf8");
-     Card arr[] = {{VERMELHO, "2", 3, 2}, {AZUL, "3", 4, 3}, {VERDE, "3", 1, 3}, {VERMELHO, "1", 3, 1}, {VERDE, "1", 1, 1}};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    SortingPayload payload = bubbleSort(arr, n);
-
-    printf("Bubble Sort:\n ");
-    showSortingPayload(&payload);
-    
-    /*printf("Bubble Sort");
-    for (int x = 0; x < n; x ++){
-        showCard(arr);
-    }*/
-    
+    showInitialMenu();
 }
