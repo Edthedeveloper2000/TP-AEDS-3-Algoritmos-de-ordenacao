b2a2971718036af67d8efe09c0624c730b256cb0
Ed-Games <edmarcosfilho2015@gmail.com>
2023-11-25 19:27:14 -0300

    refactor: removidos todos os sortings e prints de resultados e utilizada a função sort em lugar

--- src/menu.c
+++ src/menu.c
@@ -2,12 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "menu.h"
-#include "./sorting/bubbleSort/bubble.h"
-#include "./sorting/selectSort/select.h"
-#include "./sorting/InsertSort/insert.h"
-#include "./sorting/shellSort/shell.h"
-#include "./sorting/quickSort/quick.h"
-#include "./sorting/heapSort/heap.h"
+#include "./sorting/sort.h"
 
 
 void showInitialMenu() {
@@ -35,65 +30,11 @@ void startInteractiveMode() {
         cards[i] = currentCard;
     }
 
-    printf("-------------------------\n");
-    printf("MÂO INICIAL: \n");
-    printArray(cards, 10);
-    printf("-------------------------\n");
+    sort(cards);
 
-    /* SELECT SORT */
-    Card* cardsSelect = copyArray(cards, 10);
-    SortingPayload SelectPayload = SelectSort(cardsSelect, 10);
-    printf("Select Sort:\n ");
-    showSortingPayload(&SelectPayload);
-    free(cardsSelect);
-
-    /* BUBBLE SORT */
-    Card* cardsBubble = copyArray(cards, 10);
-    SortingPayload payload = bubbleSort(cardsBubble, 10);
-    free(cardsBubble);
-    printf("Bubble Sort:\n ");
-    showSortingPayload(&payload);
-
-    /* INSERT SORT */
-    Card* cardsInsert = copyArray(cards, 10);
-    SortingPayload InsertPayload = insertSort(cardsInsert, 10);
-    printf("Insert Sort:\n ");
-    showSortingPayload(&InsertPayload);
-    free(cardsInsert);
-
-    /* SHELL SORT */
-    Card* cardsShell = copyArray(cards, 10);
-    SortingPayload ShellPayload = shellSort(cardsShell, 10);
-    printf("Shell Sort:\n ");
-    showSortingPayload(&ShellPayload);
-    free(cardsShell);
-
-    /* QUICK SORT */
-    Card* cardsQuick = copyArray(cards, 10);
-    SortingPayload QuickPayload = quickSortWrapper(cardsQuick, 10);
-    printf("Quick Sort:\n ");
-    showSortingPayload(&QuickPayload);
-    free(cardsQuick);
-
-    /* HEAP SORT */
-    Card* cardsHeap = copyArray(cards, 10);
-    SortingPayload HeapPayload = heapSort(cardsHeap, 10);
-    printf("Heap Sort:\n ");
-    showSortingPayload(&HeapPayload);
-    free(cardsHeap);
 
 }
 
 void startFileMode() {
 
-}
-
-// Cópia das cartas iniciais
-Card* copyArray(const Card* source, int size) {
-    Card* copy = malloc(size * sizeof(Card));
-    if (copy == NULL) {
-        exit(EXIT_FAILURE);
-    }
-    memcpy(copy, source, size * sizeof(Card));
-    return copy;
 }
