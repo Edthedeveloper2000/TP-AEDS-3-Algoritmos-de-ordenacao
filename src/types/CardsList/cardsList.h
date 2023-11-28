281fa8af8198f09c541f49f05ef4f4e4533ad7d8
Ed-Games <edmarcosfilho2015@gmail.com>
2023-11-28 12:44:39 -0300

    style: adicionados comentarios relacionados as funções da cardList

--- src/types/CardsList/cardsList.h
+++ src/types/CardsList/cardsList.h
@@ -3,12 +3,27 @@
 #ifndef CARD_LIST_H
 #define CARD_LIST_H
 
-#define DECK_SIZE 56
+#define DECK_SIZE 56 // tamanho do baralho
 
+/**
+ * Cria um baralho de 56 cartas de UNO
+ * @param cardlist - array que irá receber as cartas do baralho
+*/
 void createCards(Card *cardslist);
 
+/**
+ * Cria um baralho aleatório de 56 cartas de UNO
+ * @param cardlist - array que irá receber as cartas do baralho
+*/
 void createRandomDeck(Card *cardsList);
 
+/**
+ * Seleciona o número informado de cartas de um baralho e povoa 
+ * a lista de cartas passada como destino com elas
+ * @param sourceDeck - array do qual as cartas serão selecionadas
+ * @param destinyDeck - array que irá receber as cartas
+ * @param amount - quantidade de cartas
+*/
 void selectAmountCards(Card* sourceDeck, Card* destinyDeck, int amount);
 
 
