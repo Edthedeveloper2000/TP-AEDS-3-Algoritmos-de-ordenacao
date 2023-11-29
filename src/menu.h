6f1c837ba844198ff0be1725b3b4a88f5b294bf6
Ed-Games <edmarcosfilho2015@gmail.com>
2023-11-28 21:08:01 -0300

    style: adicionados comentários as funções do menu

--- src/menu.h
+++ src/menu.h
@@ -1,8 +1,21 @@
 #include "./types/Card/card.h"
 #include "./types/CardsList/cardsList.h"
 
+/**
+ * Mostra o menu inicial com a opção de
+ * escolher o modo de ordenação
+*/
 void showInitialMenu();
 
+/**
+ * Inicia o modo aleatório, pelo qual as
+ * cartas a serem ordenadas são geradas
+ * aleatóriamente
+*/
 void startInteractiveMode();
 
+/**
+ * Inicia o modo de arquivo, no qual as cartas
+ * a serem ordenadas são lidas pelo arquivo especificado.
+*/
 void startFileMode();
