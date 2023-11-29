e73bb782769f6323476e48952d4c4c68047ed9e6
Ed-Games <edmarcosfilho2015@gmail.com>
2023-11-28 21:34:27 -0300

    style: adicionados comentários da função

--- src/utils/copyArray/copyArray.h
+++ src/utils/copyArray/copyArray.h
@@ -1,3 +1,9 @@
 #include "../../types/Card/card.h"
 
+/**
+ * Cria uma copia do array passado
+ * @param source - array de origem
+ * @param size - tamanho do array
+ * @return Card* - retorna um novo vetor identico ao primeiro
+*/
 Card* copyArray(const Card* source, int size);
--- src/utils/swap/swap.h
+++ src/utils/swap/swap.h
@@ -3,6 +3,12 @@
 #ifndef SWAP
 #define SWAP
 
+/**
+ * Troca o contéudo apontado por dois ponteiros de carta
+ * Na prática, troca duas cartas dentro de uma lista
+ * @param card_1 - Carta a ser trocada
+ * @param card_2 - Carta a ser trocada 
+*/
 void swap(Card *card_1, Card *card_2);
 
 #endif //SWAP
