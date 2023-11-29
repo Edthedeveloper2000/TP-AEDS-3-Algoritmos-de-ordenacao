1d3be104c5ef88abdc1ef0306fcf7b8425b08fe5
Ed-Games <edmarcosfilho2015@gmail.com>
2023-11-28 21:35:38 -0300

    style: adicionados comentarios nas funções do payload

--- src/types/SortingPayload/sortingPayload.h
+++ src/types/SortingPayload/sortingPayload.h
@@ -14,12 +14,46 @@ typedef struct {
 
 } SortingPayload;
 
-void printArray(Card arr[], int size);
-void printArrayFile(Card arr[], int size, FILE* output);
+/**
+ * Itera sobre uma lista de cartas e
+ * mostra cor e valor de cada uma
+ * @param cards[] - Lista de cartas a ser exibida no terminal
+ * @param size - tamanho da lista
+*/
+void showCards(Card cards[], int size);
 
+/**
+ * Itera sobre uma lista de cartas e
+ * mostra cor e valor de cada uma
+ * @param cards[] - Lista de cartas a ser no arquivo de saida
+ * @param size - tamanho da lista
+ * @param output - arquivo de saida
+*/
+void showCardsFile(Card cards[], int size, FILE* output);
+
+/**
+ * Cria um SortingPayload com os dados fornecidos
+ * @param payload - O ponteiro que guardará a instãncia do SortingPayload
+ * @param size - Tamanho do vetor ordenado
+ * @param moves - quantidade de movimentos
+ * @param comparisions - Quantidade de comparações
+ * @param time - tmepo gasto
+ * @param cards[] - A lista ordenada.
+*/
 void createSortingPayload( SortingPayload *payload, int size, int moves, int comparisions, double time, Card cards[]);
 
+/**
+ * Exibe na saida padrão [ terminal ] os dados do payload,
+ * como movimentos, comparações e tempo de execução
+ * @param payload - o payload a ser visualizado
+*/
 void showSortingPayload( SortingPayload *payload );
 
+/**
+ * Exibe no arquivo de saida os dados do payload,
+ * como movimentos, comparações e tempo de execução
+ * @param payload - o payload a ser visualizado
+ * @param output - o arquivo de saida
+*/
 void showSortingPayloadFile( SortingPayload *payload, FILE* output);
 #endif // SORTING_PAYLOAD_H
